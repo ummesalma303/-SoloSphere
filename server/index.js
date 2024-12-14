8b5cd8589235ffc7bde6dfbdadbaf1967d8f4d14
ummesalma303 <ummesalma333999@gmail.com>
2024-12-14 16:08:37 +0600

    Set env file

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
-
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot76b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// SoloSphere
+// JM4hTVyEBiyYbgRC
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
